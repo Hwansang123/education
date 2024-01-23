54bbea8cd8c3434c9b35b836b2523417a308d069
Hwansang123 <153247958+Hwansang123@users.noreply.github.com>
2024-01-23 18:17:14 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+   // ifstream file;
+   // file.open("/home/aiot6/CProjects/poject/004.JOBIN/logo");
+   // if(!file.is_open())
+   // {
+   //    cout<<"나 지금 거기 없다."<<endl;
+   //    return 0;
+   // }
+   // while (!file.eof())
+   // {
+   //    string door;
+   //    getline(file, door);
+   //    cout<<door<<endl;
+   // }
+   // file.close();
+   // sleep(1);
+   // cout<<"┌──────────────────────────────────┐"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"│    1. 로그인      2.회원가입     │"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"│                                  │"<<endl;
+   // cout<<"└──────────────────────────────────┘"<<endl;
+   // cout<<"원하는 메뉴를 선택해주세요.";
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // sqlite3 * DB;
+    // int job;
+    // job = sqlite3_open("/home/aiot6/CProjects/edu/24/01/job.db", &DB);
+    // if(job)
+    // {
+    //     fprintf(stderr, "나없따? : %s\n",sqlite3_errmsg(DB));
+    //     return 0;
+    // }
+    
+    // sqlite3_close(DB);
+}
