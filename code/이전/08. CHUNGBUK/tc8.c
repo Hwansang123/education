0cba25eb44e169b0444bf0d9a513b26cafb38204
Hwansang123 <153247958+Hwansang123@users.noreply.github.com>
2024-05-03 10:59:15 +0900

    Add files via upload

---
+++ code/이전/08. CHUNGBUK/tc8.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+
+int main(int argc, char* argv[])
+{
+    //ip, 포트번호 지정
+    argc=3; argv[1]="10.10.20.98"; argv[2]="9106";
+    int sock;
+    char message[BUF_SIZE];
+    FILE * file;
+    struct sockaddr_in serv_addr;
+    
+    pthread_t t_id;
+
+    if(argc!=3)
+    {
+        printf("Usage: %s <IP> <PORT>\n",argv[0]);
+        exit(1);
+    }
+    
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+    serv_addr.sin_port=htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+        error_handling("connect() error");
+
+    file=fopen("Chungbuk_2013.csv","rtwt");
+    // filesz=sizeof(file);
+    while(1)
+    {
+        fgets(message, BUF_SIZE, file);
+        if(feof(file)) break;
+        // void* arg[2]={sock,message};
+        write(sock, message, BUF_SIZE);
+        read(sock, message, BUF_SIZE);
+        memset(message, 0, sizeof(message));
+    }
+    file=fopen("Chungbuk_2014.csv","rtwt");
+    // filesz=sizeof(file);
+    while(1)
+    {
+        fgets(message, BUF_SIZE, file);
+        if(feof(file)) break;
+        // void* arg[2]={sock,message};
+        write(sock, message, BUF_SIZE);
+        read(sock, message, BUF_SIZE);
+        memset(message, 0, sizeof(message));
+    }
+    // if()
+    
+    // close(sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
